df9ea061c93d33a968ef588e41ef67e500c77dd8
Nikanov Pavel <term1@classroom.local>
2022-10-28 09:49:39 +0300

    Add DTO

---
+++ JAVA/model/src/main/java/DTO/AllCategories.java
@@ -0,0 +1,12 @@
+package DTO;
+
+import controller.bo.CategoryBO;
+
+import java.util.ArrayList;
+
+public class AllCategories {
+    ArrayList<CategoryBO> Categories;
+    AllCategories(){
+
+    }
+}
---
+++ JAVA/model/src/main/java/DTO/AllNews.java
@@ -0,0 +1,14 @@
+package DTO;
+
+import controller.bo.NewsBO;
+
+import java.util.ArrayList;
+
+public class AllNews {
+    ArrayList<NewsBO> News;
+
+    AllNews(){
+        
+    }
+
+}
